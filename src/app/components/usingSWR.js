30975cf86da756db23c18801da196bc0cefc99d7
vijith-wac <vijith.p@webandcrafts.com>
2024-08-06 11:23:25 +0530

    useSwr

--- src/app/components/usingSWR.js
+++ src/app/components/usingSWR.js
@@ -1,32 +1,77 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import useSWR from "swr";
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const UsingSWR = () => {
   const [page, setPage] = useState(1);
-  const [info,setInfo] = useState([])
-  let size = 2;
-  const fetchInfo = async()=>{
-    const { data, error, isLoading } = await useSWR(
-        `https://dev.vieroot.webc.in/public/api/blog/list?page=${page}&length=${size}`,
-        fetcher
-      );
-      setInfo([...info,...data])
-  }
+  const [info, setInfo] = useState([]);
+  const [isData,setIsData] = useState(true)
+  const [message,setMessage] = useState('')
+  const observerRef = useRef(null);
+  let size = 10;
 
-  useEffect(()=>{
-    fetchInfo()
-  },[])
-  
+  const { data, error, isLoading } = useSWR(
+    `https://dev.vieroot.webc.in/public/api/blog/list?page=${page}&length=${size}`,
+    fetcher,
+    { revalidateOnFocus: false }
+  );
 
 
-  console.log(info)
+  useEffect(() => {   
+    if (data) {
+      let finalData = data.data.original.data
+      if(finalData.length>0){
+        setInfo((prevInfo) => [...prevInfo, ...finalData]);
+      }else{
+        setIsData(false)
+      }
+      
+    }
+  }, [data]);
+
+  const loadMore = useCallback(() => {
+   if(isData){
+    setPage((prevPage) => prevPage + 1);
+   }else{
+    setMessage('Data completed')
+   }
+    
+  }, [isData]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          loadMore();
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, [loadMore]);
 
   return (
     <div>
-     
-     
+      {info?.map((item, index) => (
+        <div key={index}>
+          <h2>{item.title}</h2>
+          <img src={item.image_url}/>
+          </div>
+      ))}
+      <div ref={observerRef} style={{ height: '20px' }} />
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error loading data</div>}
     </div>
   );
 };
